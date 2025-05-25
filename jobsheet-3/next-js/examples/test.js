a5389abc2807da5e94178b3aab107d69b3b4e170
sandradevia <sandradeviarge@gmail.com>
2025-05-26 00:58:13 +0700

    update

---
+++ jobsheet-3/next-js/examples/test.js
@@ -0,0 +1,6 @@
+// example.test.js
+describe('Simple Test', () => {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(1 + 2).toBe(3);
+  });
+});
--- jobsheet-3/next-js/pages/weather.js
+++ jobsheet-3/next-js/pages/weather.js
@@ -1,48 +1,48 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
-const Weather = () => {
-  const [city, setCity] = useState('');
-  const [weather, setWeather] = useState(null);
-  const [error, setError] = useState(null);
+// const Weather = () => {
+//   const [city, setCity] = useState('');
+//   const [weather, setWeather] = useState(null);
+//   const [error, setError] = useState(null);
 
-  const getWeather = async () => {
-    try {
-      const res = await fetch(`/api/weather?city=${city}`);
-      const data = await res.json();
+//   const getWeather = async () => {
+//     try {
+//       const res = await fetch(`/api/weather?city=${city}`);
+//       const data = await res.json();
 
-      if (res.ok) {
-        setWeather(data);
-        setError(null);
-      } else {
-        setWeather(null);
-        setError(data.message);
-      }
-    } catch (err) {
-      setError('Error fetching data');
-    }
-  };
+//       if (res.ok) {
+//         setWeather(data);
+//         setError(null);
+//       } else {
+//         setWeather(null);
+//         setError(data.message);
+//       }
+//     } catch (error) {
+//       setError('Error fetching data');
+//     }
+//   };
 
-  return (
-    <div>
-      <h1>Cuaca</h1>
-      <input
-        type="text"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-        placeholder="Masukkan nama kota"
-      />
-      <button onClick={getWeather}>Cari</button>
+//   return (
+//     <div>
+//       <h1>Cuaca</h1>
+//       <input
+//         type="text"
+//         value={city}
+//         onChange={(e) => setCity(e.target.value)}
+//         placeholder="Masukkan nama kota"
+//       />
+//       <button onClick={getWeather}>Cari</button>
 
-      {error && <p>{error}</p>}
-      {weather && (
-        <div>
-          <h2>{weather.name}</h2>
-          <p>{weather.main.temp}°C</p>
-          <p>{weather.weather[0].description}</p>
-        </div>
-      )}
-    </div>
-  );
-};
+//       {error && <p>{error}</p>}
+//       {weather && (
+//         <div>
+//           <h2>{weather.name}</h2>
+//           <p>{weather.main.temp}°C</p>
+//           <p>{weather.weather[0].description}</p>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
 
-export default Weather;
+// export default Weather;
