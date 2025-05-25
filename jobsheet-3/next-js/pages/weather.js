f3f39c42cfe55cd1f3f1e7683903be0eaee6c975
sandradevia <144318268+sandradevia@users.noreply.github.com>
2025-05-26 00:52:00 +0700

    Update weather.js

--- jobsheet-3/next-js/pages/weather.js
+++ jobsheet-3/next-js/pages/weather.js
@@ -1,21 +1,21 @@
-export default async function handler(req, res) {
-  const { city } = req.query;
+// export default async function handler(req, res) {
+//   const { city } = req.query;
 
-  try {
-    if (!city) {
-      return res.status(400).json({ message: 'City parameter is required' });
-    }
+//   try {
+//     if (!city) {
+//       return res.status(400).json({ message: 'City parameter is required' });
+//     }
 
-    // Contoh data cuaca simulasi
-    const fakeWeatherData = {
-      name: city,
-      main: { temp: 25 },
-      weather: [{ description: 'Cerah' }],
-    };
+//     // Contoh data cuaca simulasi
+//     const fakeWeatherData = {
+//       name: city,
+//       main: { temp: 25 },
+//       weather: [{ description: 'Cerah' }],
+//     };
 
-    res.status(200).json(fakeWeatherData);
-  } catch (_error) {
-    console.error(_error);
-    res.status(500).json({ message: 'Internal server error' });
-  }
-}
+//     res.status(200).json(fakeWeatherData);
+//   } catch (_error) {
+//     console.error(_error);
+//     res.status(500).json({ message: 'Internal server error' });
+//   }
+// }
